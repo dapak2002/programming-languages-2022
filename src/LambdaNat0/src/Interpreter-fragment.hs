d8962fa2d637358b2ff04829b24ebbe951d26c21
alexhkurz <alexhkurz@gmail.com>
2022-09-27 08:23:30 -0700

    for teaching purposes

---
+++ src/LambdaNat0/src/Interpreter-fragment.hs
@@ -0,0 +1,20 @@
+# The heart of the interpreter ... all else is "boilerplate"
+
+evalCBN :: Exp -> Exp  
+evalCBN (EApp e1 e2) = case (evalCBN e1) of
+    (EAbs i e3) -> evalCBN (subst i e2 e3)
+    e3 -> EApp e3 e2
+evalCBN x = x 
+
+-- generate a fresh name
+fresh :: Exp -> String
+
+subst :: Id -> Exp -> Exp -> Exp
+subst id s (EVar id1) | id == id1 = s
+                      | otherwise = EVar id1
+subst id s (EApp e1 e2) = EApp (subst id s e1) (subst id s e2)
+subst id s (EAbs id1 e1) = 
+    -- to avoid variable capture, we first substitute id1 with a fresh name inside the body of the λ-abstraction, obtaining e2. Only then do we proceed to apply substitution of the original s for id in the body e2.
+    let f = fresh (EAbs id1 e1)
+        e2 = subst id1 (EVar f) e1 in 
+        EAbs f (subst id s e2)
